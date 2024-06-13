d7d975034cbe40d9e6c5079797de0b9dab50692a
MikaStoecker <mika.stoecker@iu-study.org>
2024-06-13 11:11:08 +0200

    Bis inkl. Übung Folie 100 SpringDataApp Kombination Controller (kein Rest), HTML-Template und Repository. Eingeben Person Name und Alter durch Form in Template, Controller steuer durch @PostMapping die Verarbeitung in einem Objekt durch das Formular ins Repo

--- packages/dspwa1022/src/main/java/org/iu/dspwa1022/springdata/controller/PersonController.java
+++ packages/dspwa1022/src/main/java/org/iu/dspwa1022/springdata/controller/PersonController.java
@@ -5,26 +5,30 @@ import java.util.List;
 import org.iu.dspwa1022.springdata.model.Person;
 import org.iu.dspwa1022.springdata.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/people")
 public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
 
-    @GetMapping
+    /*@GetMapping
     public List<Person> getAllPeople() {
         return personRepository.findAll();
-    }
+    }*/
 
+    @GetMapping
+    public String getAllPeople(Model model) {
+        model.addAttribute("people", personRepository.findAll());
+        return "people";
+    }
     @PostMapping
-    public Person createPerson(@RequestBody Person person) {
-        return personRepository.save(person);
+    public String createPerson(@ModelAttribute Person person, Model model) {
+        model.addAttribute("person", personRepository.save(person));
+        return "people";
     }
 }
--- packages/dspwa1022/src/main/java/org/iu/dspwa1022/springdata/model/Person.java
+++ packages/dspwa1022/src/main/java/org/iu/dspwa1022/springdata/model/Person.java
@@ -12,6 +12,7 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private int age;
 
     public Person() {
     }
@@ -30,5 +31,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
